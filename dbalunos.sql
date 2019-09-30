cddc181aaf5fd09d2f4342bae901bb765f2db0fc
sher221b <claudiojrs1993@gmail.com>
2019-09-29 23:18:34 -0300

    dbalunos

--- dbalunos.sql
+++ dbalunos.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE dbalunos;
+
+USE dbalunos;
+
 CREATE TABLE tbAlunos (
 id INT NOT NULL AUTO_INCREMENT,
 nome	VARCHAR(50) NOT NULL,
@@ -6,6 +10,3 @@ serie	INT(20),
 escola	VARCHAR(55),
 sexo	VARCHAR(15),
 PRIMARY KEY(id) );
-
-
-
