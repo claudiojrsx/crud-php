be453eac5dfd4e9baa894c52194866038e69ae8a
sher221b <claudiojrs1993@gmail.com>
2019-09-30 00:23:32 -0300

    date updated

--- index.php
+++ index.php
@@ -36,10 +36,12 @@
             </thead>
             <tbody>
             <?php foreach ($lista as $linha): ?>
+
                 <tr>
                     <td><a class="btn btn-link"><?php echo $linha['id'] ?></td>
                     <td><a class="btn btn-link"><?php echo $linha['nome'] ?></a></td>
-                    <td><a class="btn btn-link"><?php echo $linha['datnasc'] ?></a></td>
+                    <td><a class="btn btn-link"><?php $origDate = $linha['datnasc']; 
+                    $newDate = date("d-m-Y", strtotime($origDate)); echo $newDate; ?></a></td>
                     <td><a class="btn btn-link"><?php echo $linha['serie'] ?></td>
                     <td><a class="btn btn-link"><?php echo $linha['escola'] ?></td>
                     <td><a class="btn btn-link"><?php echo $linha['sexo'] ?></td>
